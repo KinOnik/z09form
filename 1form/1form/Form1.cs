f82955fc6ff0b5009dc7b5217330f32e1bfad23b
KinOnik <84587417+KinOnik@users.noreply.github.com>
2021-12-13 17:45:15 +0300

    Add files via upload

---
+++ 1form/1form/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                result.Text = ($"Реузльтат:\n");
+                string curPatch = Environment.CurrentDirectory;
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                if (a > b)
+                {
+                    MessageBox.Show(
+                        "Нарушен порядок ввода диапазона записываемых чисел, меняю местами",
+                        "Предупреждение",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning,
+                        MessageBoxDefaultButton.Button3);
+                    double buf = a;
+                    textBox1.Text = b.ToString();
+                    a = b;
+                    textBox2.Text = buf.ToString();
+                    b = buf;
+                }
+                double h = double.Parse(textBox3.Text);
+
+                FileStream f = new FileStream($"{curPatch}/tmp.dat", FileMode.Create);
+                BinaryWriter fOut = new BinaryWriter(f);
+                for (double i = a; i <= b; i += h)
+                {
+                    fOut.Write(i);
+                }
+                fOut.Close();
+
+                f = new FileStream($"{curPatch}/tmp.dat", FileMode.Open);
+                BinaryReader fIn = new BinaryReader(f);
+                long m = f.Length;
+
+                double startIgnor = double.Parse(textBox4.Text);
+                double stopIgnor = double.Parse(textBox5.Text);
+
+                if (startIgnor > stopIgnor)
+                {
+                    MessageBox.Show(
+                        "Нарушен порядок ввода диапазона игнорируемых чисел, меняю местамии",
+                        "Предупреждение",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning,
+                        MessageBoxDefaultButton.Button3);
+                    double buf = startIgnor;
+                    textBox4.Text = stopIgnor.ToString();
+                    startIgnor = stopIgnor;
+                    textBox5.Text = buf.ToString();
+                    stopIgnor = buf;
+                }
+                double tmpDigit = 0;
+
+
+                for (long i = 0; i < m; i += 8)
+                {
+                    f.Seek(i, SeekOrigin.Begin);
+                    tmpDigit = Math.Round(fIn.ReadDouble(), 2);
+                    if (tmpDigit >= startIgnor && tmpDigit <= stopIgnor)
+                    {
+                        result.Text += (".");
+                    }
+                    else
+                    {
+                        result.Text += ($"{tmpDigit:f2} ");
+                    }
+                }
+
+                Console.Write("\n\n");
+                fIn.Close();
+                f.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                         "Вы ввели не число",
+                         "Предупреждение",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Warning,
+                         MessageBoxDefaultButton.Button3);
+            }
+        }
+    }
+}
---
+++ 2form/2form/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _2form
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				result.Text = "";
+				string curPatch = Environment.CurrentDirectory;
+				int k1 = int.Parse(textBox1.Text);
+				int k2 = int.Parse(textBox2.Text);
+				if (k1 > k2)
+				{
+					MessageBox.Show(
+							 "нарушены границы индексов элементов(k2>k1), меняю местами",
+							 "Предупреждение",
+							 MessageBoxButtons.OK,
+							 MessageBoxIcon.Warning,
+							 MessageBoxDefaultButton.Button3);
+					int buffer = k1;
+					k1 = k2;
+					k2 = buffer;
+				}
+				string[] buf = System.IO.File.ReadAllLines($"{curPatch}/text.txt");
+				for (int i = 0; i < buf.Length; i++)
+				{
+					if (buf[i].Length >= k2)
+					{
+						result.Text += ($"Символы строки №{i + 1}: ");
+						for (int j = k1 - 1; j < k2; j++)
+						{
+							result.Text += ($"{buf[i][j]}");
+						}
+						result.Text += ($"\n");
+					}
+					else if (buf[i].Length >= k1 && buf[i].Length < k2)
+					{
+						result.Text += ($"В строке №{i + 1} меньше символов, чем вы ввели: ");
+						for (int j = k1 - 1; j < buf[i].Length; j++)
+						{
+							result.Text += ($"{buf[i][j]}");
+						}
+						result.Text += ($"\n");
+					}
+					else
+					{
+						result.Text += ($"В строке №{ i + 1} нет символов с такими индексом в строке\n");
+					}
+				}
+			}
+			catch (Exception)
+			{
+				MessageBox.Show(
+						 "Вы ввели не число",
+						 "Предупреждение",
+						 MessageBoxButtons.OK,
+						 MessageBoxIcon.Warning,
+						 MessageBoxDefaultButton.Button3);
+			}
+		}
+	}
+}
